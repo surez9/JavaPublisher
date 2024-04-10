818e18f74bd4066c8fbb106f176d8683e79e739e
Suresh Bhandari <suresh.bhandari@cotiviti.com>
2024-04-10 04:32:00 -0400

    Real time example with JSON data in message.

---
+++ src/main/java/com/demo/publisher/RealTImeExample.java
@@ -0,0 +1,34 @@
+package com.demo.publisher;
+
+import jakarta.jms.*;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.json.JSONObject;
+
+public class RealTImeExample {
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ActiveMQConnectionFactory("admin","admin","tcp://localhost:61616");
+
+        try {
+            Connection connection = factory.createConnection();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue("demo");
+
+            JSONObject json = new JSONObject();
+            json.put("from_date","01-Jan-2019");
+            json.put("to_date","31-Dec-2021");
+            json.put("email", "acb@gmail.com");
+            json.put("query", "Select * from data");
+
+            TextMessage textMessage = session.createTextMessage(json.toString());
+
+            MessageProducer producer = session.createProducer(destination);
+            producer.send(textMessage);
+
+            session.close();
+            connection.close();
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
